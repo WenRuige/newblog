e4637061109c907925294c5dd59156373c2b7515
gewenrui <941137860@qq.com>
2018-09-28 13:46:13 +0800

    add topk second methond

--- topk/main.go
+++ topk/main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	nums := []int{1, 3, 5, 2, 4, 6}
-	res := topk(nums, 3)
+	res := topk1(nums, 3)
 	fmt.Println(res)
 }
 
@@ -18,4 +18,22 @@ func topk(nums []int, k int) []int {
 	}
 	sort.Ints(nums)
 	return nums[len(nums)-k:]
+}
+
+//方法2:基于冒泡来进行部分排序
+func topk1(nums []int, k int) []int {
+	newNums := []int{}
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i; j < len(nums)-i-1; j++ {
+			if nums[j] > nums[j+1] {
+				nums[j+1], nums[j] = nums[j], nums[j+1]
+			}
+		}
+		if i < k {
+			newNums = append(newNums, nums[len(nums)-1-i])
+		} else {
+			break
+		}
+	}
+	return newNums
 }
