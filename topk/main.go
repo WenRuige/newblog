0eae907aef2a36a52c39a1e08ac9850d5bddf47b
gewenrui <941137860@qq.com>
2018-09-29 15:45:48 +0800

    add new function

--- topk/main.go
+++ topk/main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
+	"container/heap"
 	"fmt"
 	"sort"
 )
-
-func main() {
-	nums := []int{1, 3, 5, 2, 4, 6}
-	res := topk1(nums, 3)
-	fmt.Println(res)
-}
 
 // 方法1:首先进行排序增序排序,然后返回后k个数即可
 func topk(nums []int, k int) []int {
@@ -41,5 +36,35 @@ func topk1(nums []int, k int) []int {
 // 方法3:基于快排来进行topk排序
 
 func topk2() {
+
+}
+
+// 方法4 堆排序
+
+type IntHeap []int
+
+func (h IntHeap) Len() int           { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func main() {
+	h := &IntHeap{1, 4125, 1222, 12, 3, 42}
+	heap.Init(h)
+	heap.Push(h, 1)
+	fmt.Println(h.Len())
+	// pop的不是实际上堆排序的结果
+	fmt.Println(h.Pop())
 
 }
