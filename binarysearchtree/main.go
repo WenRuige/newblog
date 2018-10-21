772187dbe554ff0aa704bf4e88c03f8b677a40eb
941137860@qq.com <941137860@qq.com>
2018-10-21 16:36:53 +0800

    add binary tree

---
+++ binarysearchtree/main.go
@@ -0,0 +1,90 @@
+package main
+
+import "fmt"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+//func sortedArrayToBST(nums []int) *TreeNode {
+//	return helper(nums, 0, len(nums)-1)
+//}
+//
+//func helper(nums []int, i, j int) *TreeNode {
+//	if len(nums) == 0 || i > j || i < 0 || j >= len(nums) {
+//		return nil
+//	}
+//	if i == j {
+//		return &TreeNode{Val: nums[i]}
+//	}
+//	mid := i + (j-i)/2
+//	root := &TreeNode{Val: nums[mid]}
+//	root.Left = helper(nums, i, mid-1)
+//	root.Right = helper(nums, mid+1, j)
+//	return root
+//}
+
+func sortedArrayToBST(nums []int) *TreeNode {
+	if len(nums) == 0 {
+		return nil
+	}
+
+	return getTree(nums, 0, len(nums)-1)
+
+}
+
+func getTree(nums []int, l, r int) *TreeNode {
+	// left < right
+	if l <= r {
+		mid := (l + r) / 2
+		treenode := new(TreeNode)
+		treenode.Val = nums[mid]
+		treenode.Left = getTree(nums, l, mid-1)
+		treenode.Right = getTree(nums, mid+1, r)
+		return treenode
+	} else {
+		return nil
+	}
+}
+
+// 前序遍历二叉树
+func preorder(node *TreeNode) {
+	fmt.Println(node.Val)
+	if node.Left != nil {
+		preorder(node.Left)
+	}
+	if node.Right != nil {
+		preorder(node.Right)
+	}
+}
+
+// 中序遍历二叉树
+func inorder(node *TreeNode) {
+	if node.Left != nil {
+		inorder(node.Left)
+	}
+	fmt.Println(node.Val)
+	if node.Right != nil {
+		inorder(node.Right)
+	}
+}
+
+func posorder(node *TreeNode) {
+	if node.Left != nil {
+		posorder(node.Left)
+	}
+	if node.Right != nil {
+		posorder(node.Right)
+	}
+	fmt.Println(node.Val)
+}
+
+func main() {
+	nums := []int{-10, -3, 0, 5, 9}
+	res := sortedArrayToBST(nums)
+
+	posorder(res)
+	fmt.Println(res)
+}
--- binarytree/readme.md
+++ binarytree/readme.md
@@ -2,5 +2,73 @@
 
 每个节点有两个子树,左子树和右子树;
 
-//
+
+
+
+
+
+##### 二叉树常用的性质
+* 二叉树的第i层最多有2^(n-1)个节点
+* 二叉树如果深度为k,那么这颗树最多有2^(k-1)个节点
+* 二叉树分类:满二叉树,完全二叉树
+
+
+
+##### 满二叉树和完全二叉树
+
+* 满二叉树:满足节点数为2^(k-1),(说白了就是整棵树没有节点为空)
+* 完全二叉树:若二叉树的深度为h,满足(0~h-1)层二叉树的节点都达到最大个数,并且第h层的节点都集中在最左侧
+
+##### 二叉树的遍历
+
+* 前序遍历:根节点 -> 左子树  -> 右子树
+* 中序遍历:左子树 -> 根节点  -> 右子树
+* 后序遍历:左子树 -> 右子树  -> 根节点
+
+
+
+其中遍历方式又分为,递归遍历&非递归遍历
+
+```
+//定义一颗二叉树
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+// 前序遍历二叉树
+func preorder(node *TreeNode) {
+	fmt.Println(node.Val)
+	if node.Left != nil {
+		preorder(node.Left)
+	}
+	if node.Right != nil {
+		preorder(node.Right)
+	}
+}
+
+// 中序遍历二叉树
+func inorder(node *TreeNode) {
+	if node.Left != nil {
+		inorder(node.Left)
+	}
+	fmt.Println(node.Val)
+	if node.Right != nil {
+		inorder(node.Right)
+	}
+}
+
+func posorder(node *TreeNode) {
+	if node.Left != nil {
+		posorder(node.Left)
+	}
+	if node.Right != nil {
+		posorder(node.Right)
+	}
+	fmt.Println(node.Val)
+}
+```
+
+
 
