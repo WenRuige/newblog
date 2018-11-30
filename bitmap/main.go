d3ff8c20bff172513e421d604b1f67cf923dc29f
gewenrui <941137860@qq.com>
2018-11-30 12:03:04 +0800

    add bit map

--- bitmap/main.go
+++ bitmap/main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"bytes"
-	"os"
+	"fmt"
 )
-
-// 代表16进制
-const BitmapSize = 0x01 << 32
 
 const size = 64
 
@@ -15,7 +11,6 @@ const size = 64
 
 func NewBitMap(nbits int) *BitSet {
 	wordsLen := (nbits - 1) >> 6
-	fmt.Printf("----%d\n",wordsLen)
 	temp := BitSet(make([]uint64, wordsLen+1, wordsLen+1))
 	return &temp
 }
@@ -25,7 +20,6 @@ type BitSet []uint64
 
 //查找单词索引
 func (this *BitSet) wordIndex(bitIndex uint64) int {
-	// bitIndex >> 5  32
 	// bitIndex >> 6  64
 	return int(bitIndex >> 6)
 }
@@ -33,12 +27,7 @@ func (this *BitSet) wordIndex(bitIndex uint64) int {
 // 设置bitSet
 func (this *BitSet) set(bitIndex uint64) {
 	index := this.wordIndex(bitIndex)
-
-	fmt.Println(index)
-	os.Exit(1)
-	//相当于 n % 32 求十进制数在数组a[i]中的下标
 	//  (bitIndex & 0x1F); 0x1F = 64
-
 	//(*this)[index] = (*this)[index] | uint64(0x01) << (bitIndex %64)
 	// 或操作将对应的index 里面的值置成1
 	(*this)[index] |= uint64(0x01) << (bitIndex % 64)
@@ -48,6 +37,12 @@ func (this *BitSet) set(bitIndex uint64) {
 func (this *BitSet) display() {
 	fmt.Println(len(*this))
 
+}
+
+// 清除数据
+func (this *BitSet) clear(bitIndex uint64) {
+	index := this.wordIndex(bitIndex)
+	(*this)[index] ^= (uint64(0x01) << (bitIndex % 64))
 }
 
 func (this *BitSet) ToString() string {
@@ -67,12 +62,10 @@ func (this *BitSet) ToString() string {
 }
 
 func main() {
-	bit := NewBitMap(100000000000)
-	//bit.set(1)
-	bit.set(100)
-
-	fmt.Println(100%64, )
-	fmt.Printf("%b\n",1<<36)
-	//fmt.Println(bit.ToString())
-
+	bit := NewBitMap(10)
+	bit.set(10)
+	bit.set(63)
+	bit.set(1)
+	//bit.clear(10)
+	fmt.Println(bit.ToString())
 }
